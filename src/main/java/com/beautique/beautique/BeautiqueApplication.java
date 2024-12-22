08b68477b2875ad77122b10de284fa01e6666bae
Sreya <skatabathuni@scholastic.com>
2024-12-22 13:48:54 -0500

    add inital files

---
+++ src/main/java/com/beautique/beautique/BeautiqueApplication.java
@@ -0,0 +1,13 @@
+package com.beautique.beautique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beautique/beautique/BeautiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautique.beautique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
